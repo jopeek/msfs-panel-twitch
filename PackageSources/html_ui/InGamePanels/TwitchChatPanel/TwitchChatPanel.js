bb1936fb37a1a3fb15d20dd39383733a812f9918
Jan Ole Peek <jopeek@gmail.com>
2022-01-30 12:53:15 -0800

    ready for beta testing

--- PackageSources/html_ui/InGamePanels/TwitchChatPanel/TwitchChatPanel.js
+++ PackageSources/html_ui/InGamePanels/TwitchChatPanel/TwitchChatPanel.js
@@ -21,15 +21,17 @@
 
 function formatEmotes(text, emotes) {
     var splitText = text.split('');
-    for(var i in emotes) {
+    for (var i in emotes) {
         var e = emotes[i];
-        for(var j in e) {
+        for (var j in e) {
             var mote = e[j];
-            if(typeof mote == 'string') {
+            if (typeof mote == 'string') {
                 mote = mote.split('-');
                 mote = [parseInt(mote[0]), parseInt(mote[1])];
-                var length =  mote[1] - mote[0],
-                    empty = Array.apply(null, new Array(length + 1)).map(function() { return '' });
+                var length = mote[1] - mote[0],
+                    empty = Array.apply(null, new Array(length + 1)).map(function () {
+                        return ''
+                    });
                 splitText = splitText.slice(0, mote[0]).concat(empty).concat(splitText.slice(mote[1] + 1, splitText.length));
                 splitText.splice(mote[0], 1, '<img class="emoticon" src="http://static-cdn.jtvnw.net/emoticons/v1/' + i + '/3.0">');
             }
@@ -45,7 +47,7 @@ function formatBadges(badges) {
         if (badges['premium'] === "1") return '<img class="premium" src="https://static-cdn.jtvnw.net/badges/v1/bbbe0db0-a598-423e-86d0-f9fb98ca1933/1">';
     }
 
-    if (badges['moderator'] !== null ) {
+    if (badges['moderator'] !== null) {
         if (badges['moderator'] === "1") return '<img class="mod" src="https://static-cdn.jtvnw.net/badges/v1/3267646d-33f0-4b17-b3df-f923a41db1d0/1">';
     }
 
@@ -54,6 +56,11 @@ function formatBadges(badges) {
 
 $(function () {
     console.log("ready!");
+
+    // UPDATE THE CHANNEL NAME BELOW
+    const channelName = "CHALKONEPLAYS";
+
+    const toggleConnection = document.getElementById("toggleConnection");
 
     const client = new tmi.Client({
         options: {
@@ -68,12 +75,32 @@ $(function () {
         // 	username: 'bot-name',
         // 	password: 'oauth:my-bot-token'
         // },
-        channels: ['gladd']
+        channels: [channelName]
     });
-    client.connect().catch(console.error);
-    client.on('message', (channel, tags, message, self) => {
-        console.log(tags);
+
+    var connectionStatus = 0;
+
+    toggleConnection.addEventListener("click", () => {
         
+        if (connectionStatus === 0) {
+            client.connect().catch(console.error);
+            connectionStatus = 1;
+            $('#TwitchChatPanelList').prepend(`<li>--- CONNECTED TO ${channelName} ---</li>`);
+            $('#toggleConnection').attr('title', 'Disconnect');
+        } else {
+            client.disconnect().catch(console.error);
+            connectionStatus = 0;
+            $('#TwitchChatPanelList').prepend(`<li>--- DISCONNECTED FROM ${channelName} ---</li>`);
+            $('#toggleConnection').attr('title', 'Connect');
+        }
+
+    });
+
+    
+    
+    client.on('message', (channel, tags, message, self) => {
+        //console.log(tags);
+
         $('#TwitchChatPanelList').prepend(`<li><span style="font-weight: bold; color: ${tags['color']};">` + formatBadges(tags['badges']) + `${tags['display-name']}</span>: ` + formatEmotes(message, tags['emotes']) + '</li>');
 
         $('#TwitchChatPanelList li:gt(25)').remove();
